3ff409d46611cc77eb6b00e44436005633403e51
alexeei <megaxaser@gmail.com>
2025-09-16 23:01:40 +0300

    tweak kernel build flags, disable permissive selinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,29 +117,36 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
-    earlycon=msm_geni_serial,0xa90000 \
     kpti=off \
     loop.max_part=7 \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
     reboot=panic_warm \
     service_locator.enable=1 \
-    swiotlb=2048
+    iptable_raw.raw_before_defrag=1 \
+    ip6table_raw.raw_before_defrag=1 \
+    swiotlb=0
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x01000000
 
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 TARGET_KERNEL_CONFIG := vendor/lmi_defconfig
+TARGET_KERNEL_LLVM_BINUTILS := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM=1 \
-    LLVM_IAS=1
+    LLVM_IAS=1 \
+    QCLinker=1
 
 # Kernel Clang Flags
 KERNEL_CC := CC=clang
