bbb20cf2d3f3caa3430b87421f8df5ec94f823c4
alexeei <megaxaser@gmail.com>
2025-09-16 23:00:44 +0300

    fix pickup sensor, add new fastcharge implementation

--- device.mk
+++ device.mk
@@ -401,8 +401,12 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1 \
     libsensorndkbridge
     
 PRODUCT_PACKAGES += \
