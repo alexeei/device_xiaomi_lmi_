309cc649f9a409e7619ff202bd79f5480c8ad8e0
drkphnx <dark.phnx12@gmail.com>
2025-09-19 20:37:12 +0300

    add battery friendly pocket mode

--- device.mk
+++ device.mk
@@ -363,6 +363,12 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     DeviceParts
 
+PRODUCT_PACKAGES += \
+    PocketMode
+    ...
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-pocketmode.xml
+
 
 # Power
 PRODUCT_PACKAGES += \
