64e753dc6be28674a76e5824f6f91f0e763274d9
alexeei <megaxaser@gmail.com>
2025-10-04 11:06:24 +0300

    nuke fastcharge service-not exist

--- device.mk
+++ device.mk
@@ -238,9 +238,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-#Fastcharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.xiaomi_sm8250
 
 # HIDL
 PRODUCT_PACKAGES += \
