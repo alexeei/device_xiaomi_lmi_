dd9986a21bd7727f5db5e2dfba44202df0d42784
alexeei <megaxaser@gmail.com>
2025-10-17 22:42:50 +0300

    tmp add bypass to parts

--- device.mk
+++ device.mk
@@ -299,10 +299,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     KProfiles
 
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,true)
 
 #Kernel
 PRODUCT_ENABLE_UFFD_GC := true
+
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
 
 # Logging
 SPAMMY_LOG_TAGS := \
