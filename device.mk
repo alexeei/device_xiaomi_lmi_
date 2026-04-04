df548614e7922a62f3b0dcdff34bae8c8981f6ab
alexeei <megaxaser@gmail.com>
2026-04-04 14:29:31 +0300

    Revert "switch to perf"
    This reverts commit 8b0c9e69e1139d1f9a3a34b431533d3e0384c216.

--- device.mk
+++ device.mk
@@ -441,8 +441,11 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-     android.hardware.power-service-qti \
-     vendor.qti.hardware.perf@2.2.vendor 
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client 
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 
 $(call soong_config_set,qtipower,tap_to_wake_node,/sys/touchpanel/double_tap)
@@ -570,6 +573,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
     
 
