593780b24e1b79c847a765fe54ac198d1c97d57a
alexeei <megaxaser@gmail.com>
2026-04-25 21:03:59 +0300

    correct set of cpuset

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -110,8 +110,10 @@ case "$target" in
 	echo 0-2     > /dev/cpuset/background/cpus
 	echo 0-3     > /dev/cpuset/system-background/cpus
 	echo 4-6     > /dev/cpuset/foreground/boost/cpus
-	echo 1-6     > /dev/cpuset/foreground/cpus
-	echo 0-7     > /dev/cpuset/top-app/cpus
+	echo 0-5     > /dev/cpuset/foreground/cpus
+	echo 0-6     > /dev/cpuset/top-app/cpus
+    echo 0-1 > /dev/cpuset/restricted/cpus
+    echo 0-7 > /dev/cpuset/camera-daemon/cpus
 
 	# Turn off scheduler boost at the end
 	echo 0 > /proc/sys/kernel/sched_boost
