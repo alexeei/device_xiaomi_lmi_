f0db0c0c3f399b7b05b43d704feb229ff3c5971f
alexeei <megaxaser@gmail.com>
2026-04-26 21:28:56 +0300

    fix flashlight strength

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -7,6 +7,7 @@
 #include "CameraProviderExtension.h"
 
 #include <fstream>
+#include <unistd.h>
 
 #define TORCH_BRIGHTNESS "brightness"
 #define TORCH_MAX_BRIGHTNESS "max_brightness"
@@ -23,6 +24,7 @@ template <typename T>
 static void set(const std::string& path, const T& value) {
     std::ofstream file(path);
     file << value;
+    file.flush();
 }
 
 /**
@@ -50,24 +52,28 @@ int32_t getTorchDefaultStrengthLevelExt() {
 }
 
 int32_t getTorchMaxStrengthLevelExt() {
-    // 200 (out of 500) is a sane max brightness
     return 300;
 }
 
 int32_t getTorchStrengthLevelExt() {
     // We write same value in the both LEDs,
     // so get from one.
-    auto node = "/sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-05/c440000.qcom,spmi:qcom,pm8150l@5:qcom,leds@d300/leds/flashlight/brightness";
-    return get(node, 0);
+    return get(kTorchLedPaths[0], 0);
 }
 
 void setTorchStrengthLevelExt(int32_t torchStrength, bool enabled) {
+    // The PMIC LED driver needs a short delay between disabling the switch,
+    // updating flashlight current, and enabling the switch again. Without this
+    // delay the new brightness can be ignored on lmi.
     set(TOGGLE_SWITCH, 0);
-        auto node = "/sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-05/c440000.qcom,spmi:qcom,pm8150l@5:qcom,leds@d300/leds/flashlight/brightness";
-        set(node, torchStrength);
+    usleep(10000);
 
-    if (enabled)
+    set(kTorchLedPaths[0], torchStrength);
+    usleep(1000);
+
+    if (enabled) {
         set(TOGGLE_SWITCH, 255);
+    }
 }
 
 void setTorchModeExt(bool enabled) {
